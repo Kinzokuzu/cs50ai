44be25310773a05a4bf5986f2f283ee1d2576116
Kinzo <sauljcervantes2@gmail.com>
2024-01-03 19:41:19 -0800

    need to implement minmax()

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,7 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
-import re
+import copy
 
 # possible moves
 X = "X"
@@ -26,13 +26,13 @@ def player(board):
     """
     # count all instances of EMPTY
     empty_count = 0
-    for row in board:
-        for col in row:
-            if col == "EMPTY":
+    for r in range(3):
+        for c in range(3):
+            if board[r][c] == None:
                 empty_count += 1
 
     # even # of EMPTY's denotes player 'O' has just moved
-    if empty_count % 2: return 'X'
+    if empty_count % 2 == 1: return 'X'
     return 'O'
 
 
@@ -41,10 +41,11 @@ def actions(board):
     Returns set of all possible actions (i, j) available on the board.
     """
     possible_actions = set()
-    for r in range(len(board)): # rows
-        for c in range(len(board[r])): # colomns
-            if board[r][c] == "EMPTY":
-                possible_actions.add(board[r][c])
+    for r in range(3): # rows
+        for c in range(3): # colomns
+            if board[r][c] == None:
+                # add (r, c) as a tuple
+                possible_actions.add((r, c))
 
     return possible_actions
 
@@ -54,13 +55,13 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     # check for valid aciton
-    if board[action[0]][action[1]] != "EMPTY":
+    if action not in actions(board):
         raise NameError("Invalid action")
 
     # get current player
     curr_player = player(board)
     # create deep copy
-    new_board = board.deepcopy()
+    new_board = copy.deepcopy(board)
     # set new action with current player
     new_board[action[0]][action[1]] = curr_player
 
@@ -73,17 +74,17 @@ def winner(board):
     """
     # check row wins
     for r in range(3):
-        if 'X' in (board[r][0], board[r][1], board[r][2]): return 'X'
-        if 'O' in (board[r][0], board[r][1], board[r][2]): return 'O'
-    # check for column wins
+        if (board[r][0] == board[r][1] == board[r][2] == 'X'): return 'X'
+        if (board[r][0] == board[r][1] == board[r][2] == 'O'): return 'O'
+    # check column wins
     for c in range(3):
-        if 'X' in (board[0][c], board[1][c], board[2][c]): return 'X'
-        if 'O' in (board[0][c], board[1][c], board[2][c]): return 'O'
-    # check for diagnal wins
-    if 'X' in (board[0][0], board[1][1], board[2][2]): return 'X'
-    if 'O' in (board[0][0], board[1][1], board[2][2]): return 'O'
-    if 'X' in (board[0][2], board[1][1]. board[2][0]): return 'X'
-    if 'O' in (board[0][2], board[1][1]. board[2][0]): return 'O'
+        if (board[0][c] == board[1][c] == board[2][c] == 'X'): return 'X'
+        if (board[0][c] == board[1][c] == board[2][c] == 'O'): return 'O'
+    # check diagonal wins
+    if (board[0][0] == board[1][1] == board[2][2] == 'X'): return 'X'
+    if (board[0][0] == board[1][1] == board[2][2] == 'O'): return 'O'
+    if (board[0][2] == board[1][1] == board[2][0] == 'X'): return 'X'
+    if (board[0][2] == board[1][1] == board[2][0] == 'O'): return 'O'
 
     return None
 
@@ -101,7 +102,7 @@ def utility(board):
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
     our_winner = winner(board)
-    if our_winner == 'X': return -1
+    if our_winner == 'X': return 1
     if our_winner == 'O': return -1
     return 0
 
@@ -109,4 +110,3 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
